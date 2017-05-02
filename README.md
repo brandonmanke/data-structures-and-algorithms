360f6542dab8142adb91e82f1cf23751f89dbcde
Brandon Manke <me@brandonmanke.com>
2017-05-02 16:38:35 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Data Structures & Algorithms Practice
+
+This is a repository for practicing data structures and algorithms with a variety of languages.
---
+++ javascript/src/linkedlist.js
@@ -0,0 +1,14 @@
+/**
+ * Linked list implementation in vanilla JavaScript
+ * @author Brandon Manke
+ * @file linkedlist.js
+ */
+ function LinkedList() {
+
+    function node(data, next) {
+        var data = data;
+        var next = next;
+    }
+
+    var head = node(null, null);
+ }
